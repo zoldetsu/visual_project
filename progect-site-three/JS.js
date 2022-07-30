e3cbf0d9a778acc665cf4ea48d73f0a33c1657cc
nazartu <nzr.bones@gmail.com>
2022-07-30 14:56:04 +0300

    zold

---
+++ progect-site-three/JS.js
@@ -0,0 +1,11 @@
+const div = document.createElement('div');
+div.classList.add('wrapper');
+
+const body = document.body;
+body.appendChild(div);
+
+
+
+const headerr = document.createElement('h1');
+headerr.textContent = 'zoloto';
+div.insertAdjacentElement('beforebegin'.headerr);
---
+++ progect_js_1/javascript/java.js
@@ -0,0 +1,7 @@
+const div = document.createElement('div');
+console.log(div);
+
+div.classList.add('wrapper');
+
+const body = document.body;
+body.appendChild(div);
