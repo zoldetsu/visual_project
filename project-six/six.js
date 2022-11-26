61d9cc3849d00567d37abe998b80991b7361484e
nzr.bones@gmail.com <nzr.bones@gmail.com>
2022-11-26 20:45:58 +0300

    zx

--- project-six/six.js
+++ project-six/six.js
@@ -2,6 +2,7 @@ let menuBtn = document.querySelector('.menu-btn');
 let navig = document.querySelector('.navig');
 let profile = document.querySelector('.profile');
 
+
 menuBtn.addEventListener('click',() => {
     menuBtn.classList.toggle('active');
     navig.classList.toggle('active');
