c8444ca92bd7b63df3240f3df5d0491306dd7524
nzr.bones@gmail.com <nzr.bones@gmail.com>
2023-03-23 20:44:14 +0300

    gg

---
+++ javascr/js1.js
@@ -0,0 +1,11 @@
+let first = prompt('ввидите 1 число');
+let two = prompt('введите 2 число');
+
+let itog = first + two;
+
+if (itog > 10) {
+    alert('больше 10');
+
+} else {
+    alert('меньше 10');
+};
--- project-six/six.js
+++ project-six/six.js
@@ -3,6 +3,8 @@ let navig = document.querySelector('.navig');
 let profile = document.querySelector('.profile');
 
 
+
+
 menuBtn.addEventListener('click',() => {
     menuBtn.classList.toggle('active');
     navig.classList.toggle('active');
